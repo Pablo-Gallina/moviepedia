ef54549df72bf4ac67fab65da5bf9be4329e3d29
Pablo Gallina <pablogallinag@gmail.com>
2022-01-08 20:56:52 -0600

    Peticiones a la api y mostrar resultados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import getMovies from "./services/getMovies";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  // funcion del useEffect
+  const cargarDatos = ()=>{
+    getMovies().then( movie => setMovies(movie))
+  }
+  // Se ejecuta cada vez que se renderiza el componente
+  // Efecto recibe 2 parametros (funcion a ejecutar, y el elemento por el que esperaremos el cambio)
+  useEffect(cargarDatos, [])
+
+  return ( 
+    <div>
+      <section>
+        { movies.map( singleMovie => 
+          <div key={ singleMovie.id }>
+            <h4>{ singleMovie.title }</h4>
+            <p>{ singleMovie.id }</p>
+            <img src={`https://image.tmdb.org/t/p/original/${singleMovie.poster_path}`} alt="img" width="20%"/>
+            <h5> { singleMovie.release_date } </h5>
+          </div>
+        ) }
+      </section>
     </div>
   );
 }
 
-export default App;
+export default App
+
---
+++ src/services/getMovies.js
@@ -0,0 +1,19 @@
+const apiKey = 'd1995f9cb803e3952498564c2eee810a'
+
+export default function getMovies ({ keywords = 'top_rated' } = {}) {
+    const apiURL = `https://api.themoviedb.org/3/movie/${keywords}?api_key=${apiKey}&language=es-ES&page=1`;
+    return fetch(apiURL)
+        .then((res) => res.json())
+        .then(response=>{
+          // Desestrucutrar la data
+          const { results = [] } = response;
+
+          //Recorrer la data para traer solo ciertos campos
+          const allMovies = results.map(data => {
+            const { id, title, release_date, poster_path, overview } = data;
+            return { id, title, release_date, poster_path, overview };
+          });
+          console.log(allMovies);
+          return allMovies;
+        })
+}
