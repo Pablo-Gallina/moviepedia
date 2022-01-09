05bcddb8cf9d6c6711607aaf58d6fb6a0520cb1b
Pablo Gallina <pablogallinag@gmail.com>
2022-01-08 22:20:54 -0600

    Loading simple

--- src/components/ListOfMovies.jsx
+++ src/components/ListOfMovies.jsx
@@ -6,14 +6,23 @@ import MovieCard from "./Card/MovieCard";
 const ListOfMovies = ({ params }) => {
     const { keyword } = params;
     const [movies, setMovies] = useState([]);
+    // pantalla de cargando
+    const [loading, setLoading] = useState(true);
 
     // funcion del useEffect
     const cargarDatos = ()=>{
-        getMovies({ keyword }).then( movie => setMovies(movie))
+        setLoading(true);
+        getMovies({ keyword }).then( movie => {
+            setMovies(movie)
+            setLoading(false);
+        })
     }
     // Se ejecuta cada vez que se renderiza el componente
     // Efecto recibe 2 parametros (funcion a ejecutar, y el elemento por el que esperaremos el cambio)
     useEffect(cargarDatos, [keyword])
+
+    // Si esta cargando, mostrar el componente de cargar
+    if (loading) return <h1>Cargando...</h1> 
 
     return (
         movies.map( ({ id, title, poster_path, release_date }) => 
