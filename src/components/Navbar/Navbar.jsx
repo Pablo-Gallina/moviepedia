8fe9cc71aa223f1f328ce548ffab6982385fc0c8
Pablo Gallina <pablogallinag@gmail.com>
2022-01-14 23:46:41 -0600

    Buscar con el form del navbar y link al inicio al hacer clic sobre el logo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
+import {  Link ,useLocation } from "wouter";
 import logo from './logo.svg';
 
 const Navbar = () => {
+    const [keyword, setKeyword] = useState('');
+
+    const [path, pushLocation] = useLocation(); // path: locaion actual, pushLocation: locacion nueva
+
+    const handleChange = e => {
+        setKeyword(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        pushLocation(`/search/${keyword}`); // Nueva locacion
+        // setKeyword('')
+    }
     return (
         <nav className="navbar blur">
-            <img src={logo} alt="moviepedia logo" />
+            <Link to='/'>
+                <img src={logo} alt="moviepedia logo" />
+            </Link>
             <div className="el-r">
-                <form className="search">
+                <form className="search" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="search">
                             <i className="fas fa-search"></i>
                         </label>
 
-                        <input type="text" id="search" autoComplete='off' placeholder="Buscar Pelicula"/>
+                        <input type="text" id="search" onChange={handleChange} value={keyword} spellCheck='false' autoComplete='off' placeholder="Buscar Pelicula"/>
                     </div>
                 </form>  
                 <i className="fas fa-ellipsis-v"></i>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,31 +1,15 @@
-import React, { useState } from 'react'
-import { useLocation } from "wouter";
+import React from 'react'
 import ListOfMovies from 'components/ListOfMovies/ListOfMovies';
 import Spinner from 'components/Spinner/Spinner';
 import useMovies from 'hooks/useMovies';
 import TrendingMovies from 'components/TrendingSearch';
 
 const Home = () => {
-    const [keyword, setKeyword] = useState('');
-
-    const [path, pushLocation] = useLocation(); // path: locaion actual, pushLocation: locacion nueva
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        pushLocation(`/search/${keyword}`); // Nueva locacion
-    }
-    
-    const handleChange = e => {
-        setKeyword(e.target.value)
-    }
 
     const {loading, movies} = useMovies()
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} value={keyword} />
-            </form>
 
             <h1>Ultima busqueda</h1>
             { loading ? <Spinner/> : <ListOfMovies movies={movies}/>}
